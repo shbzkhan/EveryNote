df1d0a36bdec18688ff9516af53e0082be7bbe52
Bereket Engida <bekacru@gmail.com>
2024-11-07 01:57:16 +0300

    feat: middleware

--- packages/auth/src/middleware.ts
+++ packages/auth/src/middleware.ts
@@ -4,15 +4,18 @@ import { NextRequest, NextResponse } from 'next/server';
 export const client = createAuthClient();
 
 export async function authMiddleware(request: NextRequest) {
-    const { data: session } = await client.getSession({
-        fetchOptions: {
-            headers: {
-                cookie: request.headers.get('cookie') || ""
-            }
-        }
-    })
-    if (session) {
-        return NextResponse.next()
-    }
-    return NextResponse.redirect("/")
+    /**
+     * This is an example of how you can use the client to get the session
+     * from the request headers.
+     * 
+     * You can then use this session to make decisions about the request
+     */
+    // const { data: session } = await client.getSession({
+    //     fetchOptions: {
+    //         headers: {
+    //             cookie: request.headers.get('cookie') || ""
+    //         }
+    //     }
+    // })
+    return NextResponse.next();
 }
